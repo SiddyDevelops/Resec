0cfb12893e5f6dda8d89b681ffc24e6d542e6cbc
SiddyDevelops <siddhartsingh2002@gmail.com>
2022-05-27 16:32:37 +0530

    Added Coroutine Async

--- app/src/main/java/com/siddydevelops/sms_kotlin/data/BroadcastUser.kt
+++ app/src/main/java/com/siddydevelops/sms_kotlin/data/BroadcastUser.kt
@@ -6,38 +6,63 @@ import com.siddydevelops.sms_kotlin.MainActivity
 import com.siddydevelops.sms_kotlin.utils.Constants
 import com.siddydevelops.sms_kotlin.utils.actions.GetContacts
 import com.siddydevelops.sms_kotlin.utils.actions.SendSMS
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.launch
 
-class BroadcastUser(context: Context,message: String, phoneNumber: String) {
+class BroadcastUser(contextIn: Context, messageIn: String, phoneNumberIn: String) {
+
+    private var myUser: User = User("","")
+    private var context: Context
+    private var message: String
+    private var phoneNumber: String
+
     init {
-        GlobalScope.launch(Dispatchers.IO) {
+//        GlobalScope.launch(Dispatchers.IO) {
+//            context.let {
+//                DataStoreManager(it).getFromDataStore().catch { e ->
+//                    e.printStackTrace()
+//                }.collect { user ->
+//                    MainActivity.setMyUser(user.userId, user.userPin)
+//                }
+//            }
+//        }
+
+        context = contextIn
+        message = messageIn
+        phoneNumber = phoneNumberIn
+
+        GlobalScope.launch(Dispatchers.Main){
+            getInitUser()
+        }
+
+    }
+
+    private suspend fun getInitUser() {
+        GlobalScope.async {
             context.let {
-                DataStoreManager(it).getFromDataStore().catch { e->
+                DataStoreManager(it).getFromDataStore().catch { e ->
                     e.printStackTrace()
-                }.collect { user->
+                }.collect { user ->
                     MainActivity.setMyUser(user.userId, user.userPin)
+                    user
                 }
             }
-        }
-
-        if(message.contains("Resec.Contact")) {
-            if(message == "Resec.Contact<${MainActivity.getMyUser()?.userId}><${MainActivity.getMyUser()?.userPin}>") {
-                GetContacts(context)
+            if (message.contains("Resec.Contact")) {
+                Log.d("UserDetails", MainActivity.getMyUser().toString())
+                if (message == "Resec.Contact<${MainActivity.getMyUser()?.userId}><${MainActivity.getMyUser()?.userPin}>") {
+                    GetContacts(context)
+                } else {
+                    SendSMS(phoneNumber, Constants.INVALID_CREDS)
+                }
             } else {
-                SendSMS(phoneNumber, Constants.INVALID_CREDS)
-            }
-        } else {
-            when(message) {
-                Constants.ACTIVE -> SendSMS(phoneNumber, Constants.SEND_ACK)
-                Constants.HELP -> SendSMS(phoneNumber, Constants.MESSAGE_ABOUT)
-                else -> {
-                    SendSMS(phoneNumber, Constants.TRY_AGAIN)
+                when (message) {
+                    Constants.ACTIVE -> SendSMS(phoneNumber, Constants.SEND_ACK)
+                    Constants.HELP -> SendSMS(phoneNumber, Constants.MESSAGE_ABOUT)
+                    else -> {
+                        SendSMS(phoneNumber, Constants.TRY_AGAIN)
+                    }
                 }
             }
         }
-
     }
 }
