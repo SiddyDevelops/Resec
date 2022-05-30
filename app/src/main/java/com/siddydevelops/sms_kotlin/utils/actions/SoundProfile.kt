32d22c3ffc3cb89ce95bb19e3239cf334a1b4cfd
SiddyDevelops <siddhartsingh2002@gmail.com>
2022-05-30 18:13:17 +0530

    Added SoundProfile-Check

--- app/src/main/java/com/siddydevelops/sms_kotlin/utils/actions/SoundProfile.kt
+++ app/src/main/java/com/siddydevelops/sms_kotlin/utils/actions/SoundProfile.kt
@@ -3,15 +3,22 @@ package com.siddydevelops.sms_kotlin.utils.actions
 import android.content.Context
 import android.content.Context.AUDIO_SERVICE
 import android.media.AudioManager
+import android.util.Log
 
 
-class SoundProfile(context: Context) {
+class SoundProfile(context: Context, phoneNumber: String, soundNormal: Boolean) {
 
     init {
-
         val audioManager: AudioManager = context.getSystemService(AUDIO_SERVICE) as AudioManager
-        audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
-
+        if(soundNormal) {
+            audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
+        } else {
+            when (audioManager.ringerMode) {
+                AudioManager.RINGER_MODE_SILENT -> SendSMS(phoneNumber,"Your device is in Silent-Mode.")
+                AudioManager.RINGER_MODE_VIBRATE -> SendSMS(phoneNumber,"Your device is in Vibrate-Mode.")
+                AudioManager.RINGER_MODE_NORMAL -> SendSMS(phoneNumber,"Your device is in Normal-Mode.")
+            }
+        }
     }
 
 }
