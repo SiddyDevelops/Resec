8c06616cb434f559ae0681ab4c4346ece8c4d191
SiddyDevelops <siddhartsingh2002@gmail.com>
2022-06-10 15:57:44 +0530

    Added ui-vm

---
+++ app/src/main/java/com/siddydevelops/sms_kotlin/ui/SettingsViewModel.kt
@@ -0,0 +1,28 @@
+package com.siddydevelops.sms_kotlin.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.siddydevelops.sms_kotlin.data.db.SettingsDatabase
+import com.siddydevelops.sms_kotlin.data.db.SettingsRepository
+import com.siddydevelops.sms_kotlin.data.db.entity.SettingsItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SettingsViewModel(application: Application): AndroidViewModel(application) {
+    val allSettings: LiveData<List<SettingsItem>>
+    private val repository: SettingsRepository
+
+    init {
+        val dao = SettingsDatabase.getDatabase(application).getSettingDao()
+        repository = SettingsRepository(dao)
+        allSettings = repository.allSettings
+    }
+
+    fun addSetting(settingsItem: SettingsItem) = viewModelScope.launch(Dispatchers.IO) { repository.insert(settingsItem) }
+
+    fun updateSetting(settingsItem: SettingsItem) = viewModelScope.launch(Dispatchers.IO) { repository.update(settingsItem) }
+
+    fun deleteSetting(settingsItem: SettingsItem) = viewModelScope.launch(Dispatchers.IO) { repository.delete(settingsItem) }
+}
