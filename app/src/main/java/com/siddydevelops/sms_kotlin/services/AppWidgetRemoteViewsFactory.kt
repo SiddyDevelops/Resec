d0d78fa8c13a2f34271a335f24c2a91251ed2b85
SiddyDevelops <siddhartsingh2002@gmail.com>
2022-06-13 15:24:21 +0530

    Added FatalWorkingWidget

--- app/src/main/java/com/siddydevelops/sms_kotlin/services/AppWidgetRemoteViewsFactory.kt
+++ app/src/main/java/com/siddydevelops/sms_kotlin/services/AppWidgetRemoteViewsFactory.kt
@@ -1,8 +1,10 @@
 package com.siddydevelops.sms_kotlin.services
 
+import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import android.util.Log
-import android.widget.AdapterView
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService.RemoteViewsFactory
 import androidx.lifecycle.LiveData
@@ -11,6 +13,7 @@ import com.siddydevelops.sms_kotlin.data.db.SettingsDAO
 import com.siddydevelops.sms_kotlin.data.db.SettingsDatabase.Companion.getDatabase
 import com.siddydevelops.sms_kotlin.data.db.SettingsRepository
 import com.siddydevelops.sms_kotlin.data.db.entity.SettingsItem
+
 
 class AppWidgetRemoteViewsFactory(private val context: Context) : RemoteViewsFactory {
 
@@ -33,6 +36,7 @@ class AppWidgetRemoteViewsFactory(private val context: Context) : RemoteViewsFac
 
     override fun onDataSetChanged() {
         allSettings = settingsRepository.allSettings
+        //getViewAt(0)
 //        allSettings.observeForever { list->
 //            updateList(list)
 //        }
@@ -42,6 +46,7 @@ class AppWidgetRemoteViewsFactory(private val context: Context) : RemoteViewsFac
         settingsItemList.clear()
         settingsItemList.addAll(newList)
         Log.d("SomeListItem",newList.toString())
+        getViewAt(0)
         onDataSetChanged()
     }
 
@@ -52,13 +57,19 @@ class AppWidgetRemoteViewsFactory(private val context: Context) : RemoteViewsFac
         return settingsItemList.size
     }
 
-    override fun getViewAt(position: Int): RemoteViews? {
-        if (position == AdapterView.INVALID_POSITION || settingsItemList.isEmpty()) {
-            return null
-        }
-
+    override fun getViewAt(position: Int): RemoteViews {
         val rv = RemoteViews(context.packageName, R.layout.widget_item_layout)
         rv.setTextViewText(R.id.soundProfileTV, settingsItemList[position].soundProfile)
+        rv.setTextViewText(R.id.ringTV, settingsItemList[position].volRing)
+        Log.d("SP:" ,settingsItemList[position].soundProfile)
+        rv.setTextViewText(R.id.mediaTV, settingsItemList[position].volMedia)
+        rv.setTextViewText(R.id.notificationTV, settingsItemList[position].volNotification)
+        rv.setTextViewText(R.id.brightnessTV, settingsItemList[position].brightness)
+        rv.setTextViewText(R.id.timePeriodTV, context.getString(
+            R.string.time_period_text,
+            settingsItemList[position].startTime,
+            settingsItemList[position].endTime
+        ))
         return rv
     }
 
@@ -67,11 +78,11 @@ class AppWidgetRemoteViewsFactory(private val context: Context) : RemoteViewsFac
     }
 
     override fun getViewTypeCount(): Int {
-        return 0
+        return 1
     }
 
     override fun getItemId(p0: Int): Long {
-        return settingsItemList.size.toLong()
+        return 1
     }
 
     override fun hasStableIds(): Boolean {
